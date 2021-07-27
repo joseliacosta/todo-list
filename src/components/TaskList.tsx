d9f8299bc03a388139fcf0eb665ffb1e4b8f1cf0
joseliacosta <joselialcosta@gmail.com>
2021-07-26 21:35:47 -0300

    style: indent previous code

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -18,13 +18,13 @@ export function TaskList() {
   function handleCreateNewTask(event: ChangeEvent<HTMLInputElement>) {
     const title = event.target.value
     setNewTaskTitle(title)
-    if(newTaskTitle.length > 0){
+    if (newTaskTitle.length > 0) {
       const newTask: Task = {
-        id: Math.random(), 
-        title: newTaskTitle, 
-        isComplete: false 
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
       }
-      const newTasks: Task[]= [...tasks, newTask]
+      const newTasks: Task[] = [...tasks, newTask]
       setTasks(newTasks)
     }
   }
@@ -45,14 +45,14 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
@@ -63,7 +63,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -75,11 +75,11 @@ export function TaskList() {
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
